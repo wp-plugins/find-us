50ee3c3f4a922634d225e3f6c1f64577e87b0453
wpdprx <wpdprx@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-08-14 16:57:32 +0000

    Changes in 1.3: - options are deleted on module deactivation - switch between EasyMode and AdvacedMode - additional AdvancedMode options: 	- set width and height 	- change the control type (large or small) 	- show map/satellite/hyprid modes - AdvancedMode display style ids for css modification.
    git-svn-id: https://plugins.svn.wordpress.org/find-us/trunk@16932 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- find-us.php
+++ find-us.php
@@ -3,7 +3,7 @@
 Plugin Name: Find Us
 Plugin URI: http://wordpress.designpraxis.at
 Description: Integrates Google Maps with your WordPress website
-Version: 1.2
+Version: 1.3
 Author: Roland Rust
 Author URI: http://wordpress.designpraxis.at
 
@@ -12,6 +12,15 @@ Get your Google API key: http://www.google.com/apis/maps/signup.html
 */
 /* 
 Changelog:
+
+Changes in 1.3:
+- options are deleted on module deactivation
+- switch between EasyMode and AdvacedMode
+- additional AdvancedMode options: 
+	- set width and height
+	- change the control type (large or small)
+	- show map/satellite/hyprid modes
+- AdvancedMode display style ids for css modification.
 
 Changes in 1.2:
 - google map API only loads when [findusmap] is detected in the post(s)
@@ -22,9 +31,18 @@ Changes in 1.1:
 
 */
 
+add_action('init', 'dprx_find_us_init_locale');
+function dprx_find_us_init_locale() {
+	$locale = get_locale();
+	$mofile = dirname(__FILE__) . "/locale/".$locale.".mo";
+	load_textdomain('dprx_find_us', $mofile);
+}
+
 /* Admin */
 if (eregi("find-us",$_REQUEST['page'])) {
-add_action('admin_head', 'dprx_find_us_js');
+	$GLOBALS['dprx_load_map'] = 1;
+	add_action('admin_head', 'dprx_find_us_init',21);
+	add_action('admin_head', 'dprx_find_us_js',22);
 }
 add_action('admin_menu', 'dprx_find_us_add_admin_pages');
 
@@ -35,12 +53,12 @@ function dprx_check_map_start() {
 	$sql = $GLOBALS['wp_the_query']->request;
 	$res = $GLOBALS['wpdb']->get_results($sql, ARRAY_A);
 	foreach($res as $r) {
-		if (strpos($r['post_content'], "[findusmap]")) {
+		preg_match("/(.*?)\[findusmap(.*)(\])(.*?)/", $r['post_content'], $match);
+		if ($match) {
 			$GLOBALS['dprx_load_map'] = 1;
 		}
 	}
 }
-
 
 add_action('wp_head', 'dprx_find_us_init',21);
 add_action('wp_head', 'dprx_find_us_js',22);
@@ -51,13 +69,36 @@ function dprx_find_us_filter_content($data) {
 	if(empty($GLOBALS['dprx_load_map'])) {
 		return $data;
 	}
-	if (strpos ( $data, "[findusmap]" ) ) {
+	
+	preg_match("/(.*?)\[findusmap(.*)(\])(.*?)/", $data, $match);
+	if ($match) {
+		$strtoreplace = $match[0];
+		$params = $match[2];
+		$params = explode("#",$params);
+		foreach($params as $p) {
+			$kv=explode(":",$p);
+			if($kv[0] == "width") {
+				$width = $kv[1];
+			}
+			if($kv[0] == "height") {
+				$height = $kv[1];
+			}
+			if($kv[0] == "ctype") {
+				$ctype = $kv[1];
+			}
+			if($kv[0] == "endlocation") {
+				$endlocation = $kv[1];
+			}
+			if($kv[0] == "mtypes") {
+				$mtypes = $kv[1];
+			}
+		}
 		$replace = "";
 		ob_start();
-		dprx_display_find_us();
+		dprx_display_find_us($width,$height,$ctype,$endlocation,$mtypes);
 		$replace = ob_get_contents();
 		ob_end_clean();
-		return str_replace("[findusmap]", $replace, $data);
+		return str_replace($strtoreplace, $replace, $data);
 	} else {
 		return $data;
 	}
@@ -68,8 +109,31 @@ function dprx_find_us_init() {
 		return;
 	}
 	
+	if (!empty($_REQUEST['dprx_find_us_mode'])) {
+		if (get_option("dprx_find_us_mode") == 1) {
+			update_option("dprx_find_us_mode",0);
+		} else {
+			update_option("dprx_find_us_mode",1);
+		}
+	}
+	
+	if (!empty($_REQUEST['dprx_find_us_setoptions'])) {
+		update_option("dprx_find_us_mtypes",$_POST['dprx_find_us_mtypes']);
+	}
+	
 	if (!empty($_POST['dprx_find_us_apikey'])) {
 		update_option("dprx_find_us_apikey",$_POST['dprx_find_us_apikey']);
+	}
+	
+	if (!empty($_POST['dprx_find_us_width'])) {
+		update_option("dprx_find_us_width",$_POST['dprx_find_us_width']);
+	}
+	
+	if (!empty($_POST['dprx_find_us_height'])) {
+		update_option("dprx_find_us_height",$_POST['dprx_find_us_height']);
+	}
+	if (!empty($_POST['dprx_find_us_ctype'])) {
+		update_option("dprx_find_us_ctype",$_POST['dprx_find_us_ctype']);
 	}
 	
 	$googlemapapi = get_option("dprx_find_us_apikey");
@@ -78,26 +142,41 @@ function dprx_find_us_init() {
 		include(dirname(__FILE__)."/find_us_mapclass.php");
 		$GLOBALS['dprx_find_us_map'] = new dprx_find_us_Map();
 	}
-	if (!empty($_POST['dprx_location'])) {
-		update_option("dprx_location",$_POST['dprx_location']);
-		$dprx_location = $_POST['dprx_location'];
-		if (!empty($dprx_location)) {
-			$GLOBALS['dprx_find_us_map']->addAddress($dprx_location);
-			if (!get_option("dprx_location_end_lat")) { update_option("dprx_location_end_lat",$GLOBALS['dprx_find_us_map']->dprx_find_us_lat()); }
-			if (!get_option("dprx_location_end_lon")) { update_option("dprx_location_end_lon",$GLOBALS['dprx_find_us_map']->dprx_find_us_lon()); }
+	if (!empty($_POST['dprx_find_us_location'])) {
+		update_option("dprx_find_us_location",$_POST['dprx_find_us_location']);
+		$dprx_find_us_location = $_POST['dprx_find_us_location'];
+		if (!empty($dprx_find_us_location)) {
+			$GLOBALS['dprx_find_us_map']->addAddress($dprx_find_us_location);
+			if (!get_option("dprx_find_us_location_end_lat")) { update_option("dprx_find_us_location_end_lat",$GLOBALS['dprx_find_us_map']->dprx_find_us_lat()); }
+			if (!get_option("dprx_find_us_location_end_lon")) { update_option("dprx_find_us_location_end_lon",$GLOBALS['dprx_find_us_map']->dprx_find_us_lon()); }
 		}
 	}
 	if (!empty($googlemapapi)) {
-	$GLOBALS['dprx_find_us_map']->mapWidth = get_option("dprx_find_us_width");
-	if (empty($GLOBALS['dprx_find_us_map']->mapWidth)) {  $GLOBALS['dprx_find_us_map']->mapWidth = '350'; }
-	$GLOBALS['dprx_find_us_map']->mapHeight = get_option("dprx_find_us_height");
-	if (empty($GLOBALS['dprx_find_us_map']->mapHeight)) {  $GLOBALS['dprx_find_us_map']->mapHeight = '250'; }
-	$GLOBALS['dprx_find_us_map']->controlType = get_option("dprx_find_us_sontrolType");
-	if (empty($GLOBALS['dprx_find_us_map']->controlType)) {  $GLOBALS['dprx_find_us_map']->controlType = 'small'; }
-	$GLOBALS['dprx_find_us_map']->showType = get_option("dprx_find_us_sontrolType");
 	if (empty($GLOBALS['dprx_find_us_map']->showType)) {  $GLOBALS['dprx_find_us_map']->showType = false; }
 	$GLOBALS['dprx_find_us_map']->setAPIKey(get_option("dprx_find_us_apikey"));
 	}
+}
+
+
+add_action('activate_find-us/find-us.php', 'dprx_find_us_activate');
+function dprx_find_us_activate() {
+	update_option("dprx_find_us_width","320");
+	update_option("dprx_find_us_height","200");
+	update_option("dprx_find_us_ctype","small");
+	update_option("dprx_find_us_mode",1);
+	update_option("dprx_find_us_mtypes",0);
+}
+
+/* options are deleted in case of plugin deactivation */
+add_action('deactivate_find-us/find-us.php', 'dprx_find_us_deactivate');
+function dprx_find_us_deactivate() {
+	delete_option("dprx_find_us_location");
+	delete_option("dprx_find_us_mode");
+	delete_option("dprx_find_us_apikey");
+	delete_option("dprx_find_us_width");
+	delete_option("dprx_find_us_height");
+	delete_option("dprx_find_us_ctype");
+	delete_option("dprx_find_us_mtypes");
 }
 
 function dprx_find_us_add_admin_pages() {
@@ -113,38 +192,51 @@ function dprx_find_us_js() {
 	$GLOBALS['dprx_find_us_map']->printGoogleJS();
 	}
 }
-function dprx_display_find_us() {
-	$dprx_location = get_option("dprx_location");
-	$GLOBALS['dprx_find_us_map']->addaddress($dprx_location);
+function dprx_display_find_us($width="",$height="",$ctype="",$endlocation="",$mtypes="") {
 	?>
-	<div style="padding:10px;">
+	<div id="dprx_find_us_map_box">
 		<div style="float:left">
 		<?php
-		if (!empty($_REQUEST['dprx_location_start'])) {
-			$GLOBALS['dprx_find_us_map']->addAddress($_REQUEST['dprx_location_start']);
+		if (empty($endlocation)) { $endlocation = get_option("dprx_find_us_location"); }
+		$GLOBALS['dprx_find_us_map']->addaddress("".$endlocation."");
+		if (empty($width)) { $width = get_option("dprx_find_us_width"); }
+		$GLOBALS['dprx_find_us_map']->mapWidth = $width;
+		if (empty($height)) { $height = get_option("dprx_find_us_height"); }
+		$GLOBALS['dprx_find_us_map']->mapHeight = $height;
+		if (empty($ctype)) { $ctype = get_option("dprx_find_us_ctype"); }
+		$GLOBALS['dprx_find_us_map']->controlType = $ctype;
+		if (empty($mtypes)) { $mtypes = get_option("dprx_find_us_mtypes"); }
+		if($mtypes == 1) {
+			$GLOBALS['dprx_find_us_map']->showType = true;
+		} else {
+			$GLOBALS['dprx_find_us_map']->showType = false;
+		}
+		
+		if (!empty($_REQUEST['dprx_find_us_location_start'])) {
+			$GLOBALS['dprx_find_us_map']->addAddress($_REQUEST['dprx_find_us_location_start']);
 			$lat = $GLOBALS['dprx_find_us_map']->dprx_find_us_lat(1);
 			$lon = $GLOBALS['dprx_find_us_map']->dprx_find_us_lon(1);
 			$GLOBALS['dprx_find_us_map']->centerMap($lat,$lon);
 		}
 		if (count($GLOBALS['dprx_find_us_map']->validPoints) > 0) {
-			//$GLOBALS['dprx_find_us_map']->dprx_find_us_directions_js();
 			$GLOBALS['dprx_find_us_map']->showMap();
 		} else {
 			_e('No valid points found. Please try again', 'dprx_find_us');
 		}
 		?>
 		</div>
-		<div style="float:left;padding:10px;">
 		
+		<div id="dprx_find_us_map_form" style="float:left;padding-left: 5px;padding-top: 5px;">
 		<form method="post" action="<?php echo $_SERVER["REQUEST_URI"]; ?>">
-			<input type="text" style="width:200px;" id="dprx_location" name="dprx_location_start" value="<?php echo $_REQUEST['dprx_location_start']; ?>" /><br />
-			<input type="submit" id="setlocation" name="setlocation" Value="<?php _e('Find your way to us &raquo;','dprx_find_us'); ?>" />	
+			<input type="text" style="width:200px;" id="dprx_form_location" name="dprx_find_us_location_start" value="<?php echo $_REQUEST['dprx_find_us_location_start']; ?>" /><br />
+			<input type="submit" id="dprx_form_setlocation" name="setlocation" Value="<?php _e('Find your way to us &raquo;','dprx_find_us'); ?>" />	
 			</p>
 			<p>
 			powered by <a href="http://wordpress.designpraxis.at">designpraxis</a>
 			</p>
 		</form>
 		</div>
+		
 		<div id="dprx_directions_div" style="clear:both;">
 		</div>
 	</div>
@@ -152,17 +244,32 @@ function dprx_display_find_us() {
 }
 
 function dprx_find_us_options_page() {
-	
 	?>
 	<div class=wrap>
 		<h2><?php _e('Find Us') ?></h2>
 	<?php
+	if (get_option("dprx_find_us_mode") == 1) {
+		?>
+			<p>
+			<?php _e("Looking for a less komplex Solution? Please swith to ","dprx_find_us"); ?>
+			<br /><a href="admin.php?page=<?php echo $_REQUEST['page']; ?>&dprx_find_us_mode=1"><?php _e("EasyMode","dprx_find_us"); ?> &raquo;</a>
+			<br /><br />
+			</p>
+		<?php
+	} else {
+		?>
+			<p>
+			<?php _e("Looking for more options? Please swith to ","dprx_find_us"); ?>
+			<br /><a href="admin.php?page=<?php echo $_REQUEST['page']; ?>&dprx_find_us_mode=1"><?php _e("AdvancedMode","dprx_find_us"); ?> &raquo;</a>
+			<br /><br />
+			</p>
+		<?php
+	}
 	?>
 	<!-- Basic Options -->
-	<form method="post" action="<?php echo $_SERVER["REQUEST_URI"]; ?>">
+	<form method="post" action="<?php bloginfo("wpurl"); ?>/wp-admin/admin.php?page=find-us/find-us.php">
 	<fieldset name="dprx_find_us_basic_options"  class="options">
-		<legend><?php _e('Enter your Google Maps API Key', 'dprx_find_us') ?></legend>
-			<label for="dprx_find_us_apikey">
+		<p><b><?php _e('Enter your Google Maps API Key', 'dprx_find_us') ?></b></p>
 				<input type="text" style="width:100%;" id="dprx_find_us_apikey" name="dprx_find_us_apikey" value="<?php echo get_option("dprx_find_us_apikey"); ?>" />
 					<p class="submit">
 					<input type="submit" id="setapikey" name="setapikey" Value="<?php _e('Set Google Maps API Key','dprx_find_us'); ?>" />	
@@ -175,30 +282,102 @@ function dprx_find_us_options_page() {
 				<?php
 				}
 				?>
-			</label>
 	</fieldset>
 	</form>
 	<?php
 	if (get_option("dprx_find_us_apikey")) {
-		$dprx_location = get_option("dprx_location");
+		$dprx_find_us_location = get_option("dprx_find_us_location");
 	?>
 	<!-- Basic Options -->
 	<form method="post" action="<?php echo $_SERVER["REQUEST_URI"]; ?>">
 	<fieldset name="dprx_find_us_basic_options"  class="options">
-		<legend><?php _e('Where can people find you?', 'dprx_find_us') ?></legend>
+		<p><b><?php _e('Where can people find you?', 'dprx_find_us') ?></b></p>
 		<?php _e('Just type in your address. In case you cannot find it here, try an address near yours and refine location search on the map.', 'dprx_find_us') ?>
-		<input type="text" style="width:400px;" id="dprx_location" name="dprx_location" value="<?php echo get_option("dprx_location"); ?>" />
+		<input type="text" style="width:400px;" id="dprx_find_us_location" name="dprx_find_us_location" value="<?php echo get_option("dprx_find_us_location"); ?>" />
 		<p class="submit">
-		<input type="submit" id="setlocation" name="setlocation" Value="<?php _e('set your location','dprx_find_us'); ?>" />	
+		<input type="submit" id="dprx_find_us_setoptions" name="dprx_find_us_setoptions" Value="<?php _e('set your location','dprx_find_us'); ?>" />	
 		</p>
 		
-		<p><?php _e('To display the "Find Us"-Map, use the following code:') ?></p>
-		<p>[findusmap]</p>
+		<?php
+		if (get_option("dprx_find_us_mode") == 1) {
+		?>
+		<p>
+		<?php _e('Map width:') ?> <input type="text" style="width:100px;" id="dprx_find_us_width" name="dprx_find_us_width" value="<?php echo get_option("dprx_find_us_width"); ?>" /> px
+		</p>
+		<p>
+		<?php _e('Map height:') ?> <input type="text" style="width:100px;" id="dprx_find_us_height" name="dprx_find_us_height" value="<?php echo get_option("dprx_find_us_height"); ?>" /> px
+		</p>
+		<p>
+		<?php _e('Control Type:') ?> <select name="dprx_find_us_ctype" id="dprx_find_us_ctype">
+		<option value="large"<?php if (get_option("dprx_find_us_ctype") == "large") { echo " selected"; } ?>><?php _e('large'); ?></option>
+		<option value="small"<?php if (get_option("dprx_find_us_ctype") == "small") { echo " selected"; } ?>><?php _e('small'); ?></option>
+		</select>
+		</p>
+		<p>
+		<?php _e('Show Map Types:') ?> <input type="checkbox" id="dprx_find_us_mtypes" name="dprx_find_us_mtypes" value="1"
+		<?php $mtypes = get_option("dprx_find_us_mtypes");
+		if ($mtypes == 1) {
+			echo " checked";
+		}?>/>
+		</p>
+		<p class="submit">
+		<input type="submit" id="dprx_find_us_setoptions" name="dprx_find_us_setoptions" Value="<?php _e('set default options','dprx_find_us'); ?>" />	
+		</p>
+		<?php
+		}
+		?>
+		<p><b><?php _e('To display the "Find Us"-Map, use the following code:') ?></b></p>
 		
-		<p><?php _e('This is what your "Find Us"-Map will look like:') ?></p>
+		<?php
+		if (get_option("dprx_find_us_mode") == 1) {
+			$tag = "[findusmap";
+			$endlocation = get_option("dprx_find_us_location");
+			if (!empty($endlocation)) {
+				$tag .= "#endlocation:".get_option("dprx_find_us_location");
+			}
+			$width = get_option("dprx_find_us_width");
+			if (!empty($width)) {
+				$tag .= "#width:".get_option("dprx_find_us_width");
+			}
+			$height = get_option("dprx_find_us_height");
+			if (!empty($height)) {
+				$tag .= "#height:".get_option("dprx_find_us_height");
+			}
+			$ctype = get_option("dprx_find_us_ctype");
+			if (!empty($ctype)) {
+				$tag .= "#ctype:".get_option("dprx_find_us_ctype");
+			}
+			$mtypes = get_option("dprx_find_us_mtypes");
+			if (!empty($mtypes)) {
+				$tag .= "#mtypes:".get_option("dprx_find_us_mtypes");
+			}
+			$tag .="]";
+			echo $tag;
+			?>
+			<p>
+			<?php _e('This is the same as using [findusmap], since without parameters specified, defaults are used','dprx_find_us'); ?>
+			<br />
+			<?php _e('Change the parameters within the tag to use custom settings for a page or post.','dprx_find_us'); ?>
+			</p>
+			<p>
+			<b><?php _e('You can use following ids to style the map display in css','dprx_find_us'); ?></b>
+			</p>
+			<p>
+			#dprx_find_us_map_box (<?php _e('for the div around map and form','dprx_find_us'); ?>)<br />
+			#dprx_find_us_map_form (<?php _e('for the floating div around the form','dprx_find_us'); ?>)<br />
+			#dprx_form_location (<?php _e('for the input field','dprx_find_us'); ?>)<br />
+			#dprx_form_setlocation (<?php _e('for the submit button','dprx_find_us'); ?>)
+			</p>
+			<?php
+		} else {
+			$tag = "[findusmap]";
+			echo $tag;
+		}
+		?>
+		<p><b><?php _e('This is what your "Find Us"-Map will look like:') ?></b></p>
 		<?php 
-		if (get_option("dprx_location")) {
-		dprx_display_find_us();
+		if (get_option("dprx_find_us_location")) {
+			echo dprx_find_us_filter_content(" ".$tag." ");
 		}
 		?>
 	</fieldset>
@@ -209,4 +388,5 @@ function dprx_find_us_options_page() {
 	</div>
 	<?php
 }
+
 ?>
